13b9b6124e056b64a3f45d243dd838a58f0f2fa8
Angela <74046447+angelaliuyl@users.noreply.github.com>
2026-06-28 23:51:55 +0100

    first draft
    still need more puzzles and to finalise order of puzzles

--- js/common.js
+++ js/common.js
@@ -1,3 +1,40 @@
 function normalise(text) {
     return text.trim().toLowerCase();
 }
+
+function enableAnswer(correctAnswers, nextPage) {
+
+    if (!Array.isArray(correctAnswers)) {
+        correctAnswers = [correctAnswers];
+    }
+
+    correctAnswers = correctAnswers.map(normalise);
+
+    const input = document.getElementById("answer");
+    const button = document.getElementById("continueButton");
+
+    input.addEventListener("input", () => {
+
+        const answer = normalise(input.value);
+
+        button.disabled = !correctAnswers.includes(answer);
+
+    });
+
+    button.addEventListener("click", () => {
+        window.location.href = nextPage + window.location.search;
+    });
+
+}
+
+function enableContinue(nextPage) {
+
+    document
+        .getElementById("continueButton")
+        .addEventListener("click", () => {
+
+            window.location.href = nextPage + window.location.search;
+
+        });
+
+}
